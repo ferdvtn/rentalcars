b11e211d7275d72b17e6ebeadc33cc53c432b431
ferdvtn <hayferdian@gmail.com>
2021-10-20 19:46:13 +0700

    - add max width sm

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mx-auto flex flex-col min-h-screen bg-gray-100 text-gray-700">
+    <div class="max-w-sm mx-auto flex flex-col min-h-screen bg-gray-100 text-gray-700">
         <TheHeader />
         <Nuxt />
         <TheFooter />
